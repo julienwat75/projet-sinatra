52e7b9a7879c0090cdb990a70fa112d7b592f7ca
julienwat75 <julien_wat@hotmail.fr>
2013-10-30 12:41:10 +0100

    editer

--- toto.rb
+++ toto.rb
@@ -8,7 +8,7 @@ set :database, "sqlite3:///foo.sqlite3"
 class User < ActiveRecord::Base
 
   def late?
-    Date.today > duedate
+    Date.today < duedate
   end
 
 end
@@ -30,6 +30,46 @@ erb :accueil
 
 end
 
+get '/listes' do
+  @users=User.all
+erb :listes
+end
+
+
+
+
+  get '/edit/:id' do
+
+
+   @user=User.find(params[:id])
+
+   erb :modifier2
+
+
+    end
+
+
+put '/edit/:id' do
+ 
+  
+ @user = User.find(params[:id])
+  
+  #@user.update_attributes(params)
+
+  @user.update_attributes(params[:user])
+
+  #@user.destroy
+
+  @users = User.all 
+
+  redirect '/listes'
+
+
+  
+
+end
+
+
 get '/clients' do
   erb:listes_clients
 
@@ -50,6 +90,13 @@ post '/save' do
   erb :listes
 
 end
+
+
+
+
+
+
+
 
 
 
